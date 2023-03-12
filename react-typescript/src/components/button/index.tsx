3d4feb7fe018499b3b58a79edb9a2b87742c6bf2
alinenink <alinenink@gmail.com>
2023-03-12 01:09:20 -0300

    initial commit

---
+++ react-typescript/src/components/button/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Button extends React.Component {
+    render() {
+     return (
+        <button>
+          Botão
+        </button>
+      )
+   }
+  }
+
+  // const Botao = React.createElement('button', {}, 'Botão');
+
+
+  export default Button; 
